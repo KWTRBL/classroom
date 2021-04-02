cb7c9f2e93019bb65e96d8adccdff5f02550dc4b
Babe <60010710@kmitl.ac.th>
2021-04-02 14:07:06 +0700

    แก้ไข download file ของระบบจัดห้องเรียน
    รวมข้อมูลบางส่วนเข้าด้วยกัน

--- classroom/server/routes/downloadfile.js
+++ classroom/server/routes/downloadfile.js
@@ -8,16 +8,16 @@ module.exports.read = async function (req, callback) {
     let semester = req.body.semester
     let curr2_id = req.body.curr2_id
     let curr2_tname = req.body.curr2_tname
-    let sql = `SELECT teach_table.subject_id as รหัสวิชา,subject.subject_ename as ชื่อวิชา,teach_table.class as ชั้นปี,teach_table.section as กลุ่ม,teacher.t_prename as คำนำหน้าชื่อ,teacher.teacher_tname as ชื่อผู้สอน,
-    teach_table.studentnum as จำนวนนศ,teach_table.room_no as ห้องเรียน,teach_table.building_no as อาคารเรียน,
-    IF(teach_table.teach_day=1,'อาทิตย์',IF(teach_table.teach_day=2,'จันทร์',
-                                            IF(teach_table.teach_day=3,'อังคาร',
-                                               IF(teach_table.teach_day=4,'พุธ',
-                                                  IF(teach_table.teach_day=5,'พฤหัสบดี', 
-                                                     IF(teach_table.teach_day=6,'ศุกร์', 
-                                                        IF(teach_table.teach_day=7,'เสาร์',teach_table.teach_day))))))) 
-    AS วันที่เรียน
-    ,teach_table.teach_time as เวลาเริ่มเรียน,teach_table.teach_time2 as เวลาเลิกเรียน,teach_table.lect_or_prac as ประเภท,teach_status as การจัดห้อง
+    let sql = `SELECT teach_table.subject_id as รหัสวิชา,subject.subject_ename as ชื่อวิชา,teach_table.class as ชั้นปี,teach_table.section as กลุ่ม,
+    CONCAT(teacher.t_prename,teacher.teacher_tname) as ชื่อผู้สอน,
+        teach_table.studentnum as จำนวนนศ,teach_table.room_no as ห้องเรียน,teach_table.building_no as อาคารเรียน,
+        CONCAT(IF(teach_table.teach_day=1,'อา.',IF(teach_table.teach_day=2,'จ.',
+                                                IF(teach_table.teach_day=3,'อ.',
+                                                   IF(teach_table.teach_day=4,'พ.',
+                                                      IF(teach_table.teach_day=5,'พฤ.', 
+                                                         IF(teach_table.teach_day=6,'ศ.', 
+                                                            IF(teach_table.teach_day=7,'ส.',teach_table.teach_day))))))) ,teach_table.teach_time,"-",teach_table.teach_time2) 
+        AS วันที่เรียน,teach_table.lect_or_prac as ประเภท
     FROM teach_table ,curriculum2,subject,teacher,teacher_teach
     WHERE teach_table.semester=? AND teach_table.year=? AND teach_table.curr2_id=? AND teach_table.subject_id=subject.subject_id AND teach_table.curr2_id=curriculum2.curr2_id AND teach_table.subject_id=teacher_teach.subject_id AND teach_table.section=teacher_teach.section AND teacher_teach.teacher_id=teacher.teacher_id 
     ORDER BY teach_table.curr2_id,teach_table.class,teach_table.subject_id,teach_table.section ASC`  // คำสั่ง sql
@@ -39,21 +39,17 @@ module.exports.read = async function (req, callback) {
                 "ชื่อวิชา",
                 "ชั้นปี",
                 "กลุ่ม",
-                "คำนำหน้าชื่อ",
                 "ชื่อผู้สอน",
                 "จำนวนนศ",
                 "ห้องเรียน",
                 "อาคารเรียน",
                 "วันที่เรียน",
-                "เวลาเริ่มเรียน",
-                "เวลาเลิกเรียน",
                 "ประเภท",
-                "การจัดห้อง",
             ]
 
-            ws.cell(1, 7).string(`ประกาศ คณะวิศวกรรมศาสตร์`)
-            ws.cell(2, 6).string(`เรื่อง ตารางเรียน - ตารางสอบ ประจำภาคเรียนที่ ${semester} ปีการศึกษา ${year}`)
-            ws.cell(3, 7).string(`ภาควิชา ${curr2_tname}`)
+            ws.cell(1, 2).string(`ประกาศ คณะวิศวกรรมศาสตร์`)
+            ws.cell(2, 2).string(`เรื่อง ตารางเรียน - ตารางสอบ ประจำภาคเรียนที่ ${semester} ปีการศึกษา ${year}`)
+            ws.cell(3, 2).string(`ภาควิชา ${curr2_tname}`)
 
             //Write Column Title in Excel file
             let headingColumnIndex = 1;
